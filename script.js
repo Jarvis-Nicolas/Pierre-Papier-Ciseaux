01d8b38d74e32af0b429acf99e6f9d15a577a341
JARVIS NICOLAS <jarvisalix@gmail.com>
2024-03-20 13:58:45 +0100

    Commit JS + ajout HTML

---
+++ script.js
@@ -0,0 +1,16 @@
+var vous;
+var votreScore = 0;
+var adversaire;
+var scoreAdversaire = 0;
+
+var choix = ["pierre","papier","ciseaux"];
+
+window.onload = function() {
+    for (let 1 = 0; i < 3; i++) {
+        // <img id="pierre" src='img/rock.png'>
+        let choix = document.createElement("img");
+        choix-m.id = choix[i];
+        choix-m.src = choix[i] + ".png";
+        document.getElementById("choix").append(choix);
+    }
+}
